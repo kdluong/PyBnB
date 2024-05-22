06ac8af0ebc43376aa412a0f68b03b50df62f758
kdluong <kdluong@ucsc.edu>
2024-05-22 15:22:36 -0700

    Replace setup.py with requirements.txt

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='PyBnB',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=['firebase-admin','pandas','selenium',],
-)
